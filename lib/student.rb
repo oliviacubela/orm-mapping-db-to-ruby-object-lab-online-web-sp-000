96589513002ca23139620ad439a3683f035110ad
Olivia Cubela <oliviacubela@gmail.com>
2020-08-28 11:22:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,8 +111,8 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql = <<-SQL 
-      SELECT * 
+    sql = <<-SQL
+      SELECT *
       FROM students
       WHERE grade != 12
     SQL
